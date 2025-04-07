ae51d169fe2091bd2f3429e58971e6ce70055643
mrglist3431 <ifedotov236@gmail.com>
2025-04-07 21:50:04 +0300

    new comment

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 #include <string>
-int
-main()
-{
-  // Моя бабушка курит трубку
-  string name;
-  std::cout << "Enter your name";
-  std::cin >> name;
-  std::cout << "Hello world from " << name << "!" << std::endl;
+
+int main(){
+//Моя бабушка курит трубку но в main
+string name;
+std::cout<<"Enter your name";
+std::cin>>name;
+std::cout<<"Hello world from "<<name<<"!"<<std::endl;
+
 }
