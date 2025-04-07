19dd4bbed738e9bd898a589f6b77417298740a1a
mrglist3431 <ifedotov236@gmail.com>
2025-04-07 21:46:23 +0300

    style changed

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,10 +1,11 @@
-#include<iostream>
+#include <iostream>
 #include <string>
-int main(){
-//Моя бабушка курит трубку
-string name;
-std::cout<<"Enter your name";
-std::cin>>name;
-std::cout<<"Hello world from "<<name<<"!"<<std::endl;
+int
+main()
+{
+  // Моя бабушка курит трубку
+  string name;
+  std::cout << "Enter your name";
+  std::cin >> name;
+  std::cout << "Hello world from " << name << "!" << std::endl;
 }
-
